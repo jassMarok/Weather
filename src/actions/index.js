7b5eccf8af694d1eec137ff980a057befc5ee696
Jaspal <jasskot18@gmail.com>
2017-12-29 03:52:36 -0500

    search bar and get request

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+const API_KEY ='b2418ccbd9b2cdda3fb9addcfb924c26';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER='FETCH_WEATHER';
+
+export function fetchWeather(city){
+	const url =`${ROOT_URL}&q=${city},ca`;
+	const request =axios.get(url);
+
+	return{
+		type:FETCH_WEATHER,
+		payload:request
+	};
+}
